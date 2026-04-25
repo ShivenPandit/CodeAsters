e803c3cf6b99d2d4b36e13c96032c9ac59cd8dcf
ShivenPandit <shivenpandit2@gmail.com>
2026-04-25 16:27:19 +0530

    feat: add Facebook Meta Pixel integration for tracking page views

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ const googleVerificationToken = process.env.GOOGLE_SITE_VERIFICATION?.trim();
 const bingVerificationToken = process.env.BING_SITE_VERIFICATION?.trim();
 const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID?.trim();
 const gtmContainerId = "GTM-M9R7GNGL";
+const metaPixelId = "2138609143647687";
 
 const metadataVerification: Metadata["verification"] = {
   google: googleVerificationToken || undefined,
@@ -135,6 +136,19 @@ gtag('config', '${gaMeasurementId}', { anonymize_ip: true });
 `
     : null;
 
+  const metaPixelScript = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${metaPixelId}');
+fbq('track', 'PageView');
+`;
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -143,12 +157,24 @@ gtag('config', '${gaMeasurementId}', { anonymize_ip: true });
         <Script id="google-tag-manager" strategy="afterInteractive">
           {gtmBootstrapScript}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {metaPixelScript}
+        </Script>
         <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${gtmContainerId}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
           />
         </noscript>
         {gaMeasurementId ? (
